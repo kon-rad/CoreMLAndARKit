940e36f7abdc0c4ea233adcc8c3e9191031db7e0
konradgnat <konradmgnat@gmail.com>
2020-03-18 12:45:44 -0500

    add model

--- CoreMLAndARKit/ViewController.swift
+++ CoreMLAndARKit/ViewController.swift
@@ -15,9 +15,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
-    private var resentModel = Resnet50()
+    private var resnetModel = Resnet50()
     
     private var hitTestResult :ARHitTestResult!
+    
+    private var visionRequests = [VNRequest]()
     
     
     override func viewDidLoad() {
@@ -93,6 +95,89 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         }
         
         self.hitTestResult = hitTestResult
+        let pixelBuffer = currentFrame.capturedImage
+        
+        performVisionRequest(pixelBuffer: pixelBuffer)
+    }
+    
+    private func displayPredicitons(text :String) {
+        
+        let node = createText(text: text)
+        
+        node.position = SCNVector3(self.hitTestResult.worldTransform.columns.3.x,
+                                       self.hitTestResult.worldTransform.columns.3.y,
+                                       self.hitTestResult.worldTransform.columns.3.z)
+        
+        self.sceneView.scene.rootNode.addChildNode(node)
+        
+        
+    }
+    
+    private func createText(text :String) -> SCNNode {
+        
+        let parentNode = SCNNode()
+        
+        // create sphere
+        let sphere = SCNSphere(radius: 0.01)
+        
+        let sphereMaterial = SCNMaterial()
+        sphereMaterial.diffuse.contents = UIColor.orange
+        sphere.firstMaterial = sphereMaterial
+        let sphereNode = SCNNode(geometry: sphere)
+        
+        // create text
+        let textGeometry = SCNText(string: text, extrusionDepth: 0)
+        
+        textGeometry.alignmentMode = CATextLayerAlignmentMode.center.rawValue
+        textGeometry.firstMaterial?.diffuse.contents = UIColor.orange
+        textGeometry.firstMaterial?.specular.contents = UIColor.white
+        textGeometry.firstMaterial?.isDoubleSided = true
+        
+        let font = UIFont(name: "Futura", size: 0.15)
+        textGeometry.font = font
+        
+        let textNode = SCNNode(geometry: textGeometry)
+        textNode.scale = SCNVector3Make(0.2, 0.2, 0.2)
+        
+        parentNode.addChildNode(sphereNode)
+        parentNode.addChildNode(textNode)
+        return parentNode
+        
+    }
+    
+    private func performVisionRequest(pixelBuffer :CVPixelBuffer) {
+        
+        let visionModel = try! VNCoreMLModel(for: self.resnetModel.model)
+        
+        let request = VNCoreMLRequest(model: visionModel) { request, error in
+            
+            if error != nil {
+                return
+            }
+            
+            guard let observations = request.results else {
+                return
+            }
+            
+            let observation = observations.first as! VNClassificationObservation
+            
+            print("Name \(observation.identifier) and confidence is \(observation.confidence)")
+            
+            DispatchQueue.main.async {
+                self.displayPredicitons(text: observation.identifier)
+            }
+        }
+        
+        request.imageCropAndScaleOption = .centerCrop
+        
+        self.visionRequests = [request]
+        
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: .upMirrored, options: [:])
+        
+        DispatchQueue.global().async {
+            try! imageRequestHandler.perform(self.visionRequests)
+        }
+        
     }
     
     
